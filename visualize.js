52dcfb5dc6a7b89dbb4daa8b27a941902ba63f76
Umesh Dinkar <udinkar@sfu.ca>
2016-01-23 18:25:40 -0800

    setup app and collect user input

---
+++ visualize.js
@@ -0,0 +1,10 @@
+(function(context){
+
+  var destination = document.getElementById('destination');
+  var visualize = document.getElementById('visualize');
+  var body = document.getElementsByTagName('body')[0];
+
+  visualize.addEventListener('click', function() {
+  	body.innerHTML = 'To be implemented...';
+  });
+})(window)
