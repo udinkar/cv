9f6c85a57cbe97fdad0fa235980343c9a2c9e06b
Umesh Dinkar <udinkar@sfu.ca>
2016-02-27 18:49:31 -0800

    Build URL from user input and XML request

--- visualize.js
+++ visualize.js
@@ -1,10 +1,31 @@
 (function(context){
+	var createURL = function() {
+		var URL;
+		// Append allowed URLs to permissions in manifest file
+		// Check the URL here against a pre-defined list
+		URL = document.getElementById("localCL").value;
+		if (document.getElementById("pic").value === "yes")
+			URL = URL + "hasPic=1&";
+		if (document.getElementById("minprice").value && (Number.parseFloat(document.getElementById("minprice").value) > 0))
+			URL = URL + "min_price=" + document.getElementById("minprice").value + "&";
+		if (document.getElementById("maxprice").value && (Number.parseFloat(document.getElementById("maxprice").value) > 0))
+			URL = URL + "max_price=" + document.getElementById("maxprice").value + "&";
+		if (document.getElementById("furnished").value === "yes")
+			URL = URL + "is_furnished=1&";
+		return URL;
+	};
+	document.getElementById('visualize').addEventListener('click', function() {
+		//var iframe = document.getElementsByTagName('iframe')[0];
+		//iframe.src("http://vancouver.craigslist.ca/search/apa?hasPic=1&min_price=500&max_price=1500&is_furnished=1")
 
-  var destination = document.getElementById('destination');
-  var visualize = document.getElementById('visualize');
-  var body = document.getElementsByTagName('body')[0];
+		var xhr = new XMLHttpRequest();
+		xhr.responseType = 'document';
 
-  visualize.addEventListener('click', function() {
-  	body.innerHTML = 'To be implemented...';
-  });
+		xhr.open('GET', createURL(), true);
+		xhr.onload = function(e) {
+			var clPage = this.response;
+			debugger;
+		};
+		xhr.send();
+	});
 })(window)
